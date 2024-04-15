ccebb89ce966eba7ea1222385c0f695da6c9a6fb
suraj-sharma-911 <surajrs911@gmail.com>
2024-04-15 15:47:52 +0530

    welcome page text change

--- app/page.tsx
+++ app/page.tsx
@@ -16,11 +16,8 @@ export default function Page() {
           <p
             className={`text-xl text-gray-800 md:text-3xl md:leading-normal ${lusitana.className}`}
           >
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
-            </a>
-            , brought to you by Vercel.
+            <strong>Welcome to Acme.</strong> This is the example for the
+            Full-stack dashboard appliation from Next.js Learn Course
           </p>
           <Link
             href="/login"
