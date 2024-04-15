2470bcfac5ddceb77c9ea2bdfd274946f2b74b45
suraj-sharma-911 <surajrs911@gmail.com>
2024-04-15 12:30:49 +0530

    added comment for middleware

--- middleware.ts
+++ middleware.ts
@@ -5,5 +5,6 @@ export default NextAuth(authConfig).auth;
 
 export const config = {
   // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
+  // to avaoid edge function error always check project setting to nextjs
   matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
 };
